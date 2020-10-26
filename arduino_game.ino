7072bc33b0d87f98c693e17b07a8824d8c6ebcdb
Marta Yurkevich <a32516@gmail.com>
2020-10-26 16:32:41 +0300

    Add game logic

--- arduino_game.ino
+++ arduino_game.ino
@@ -7,7 +7,6 @@ unsigned char screen[8][8];
 
 void setup()
 {
-  Serial.begin(9600);
   pinMode(LEFT_BUTTON, INPUT);
   pinMode(RIGHT_BUTTON, INPUT);
   digitalWrite(LEFT_BUTTON, HIGH);
@@ -25,14 +24,85 @@ void loop()
   Wait(50);
 }
 
+int jump_state = 0;  // 1,2 - 3,4
+int duck_state = 0;
+int player_pos = 3;  // 3-4
+int enemy_pos = 0;
+int enemy_type = 0;
+
 void Update()
 {
-  Serial.println(digitalRead(LEFT_BUTTON));
+  boolean jump = 1 - digitalRead(LEFT_BUTTON);
+  boolean duck = 1 - digitalRead(RIGHT_BUTTON);
+
+  if (!jump_state && !duck && jump)
+  {
+    jump_state = 1;
+  }
+
+  if (jump_state)
+  {
+    if (jump_state > 4)
+    {
+      jump_state = 0;
+    }
+    else
+    {
+      player_pos += (jump_state > 2 ? -1 : 1);
+      jump_state += 1;
+    }
+  }
+  else if (duck && !duck_state) {
+    player_pos -= 1;
+    duck_state = 1;
+  }
+  else if (!duck && duck_state) {
+    player_pos += 1;
+    duck_state = 0;
+  }
+
+  enemy_pos -= 1;
+  if (enemy_pos < 0)
+  {
+    enemy_pos = random(10, 20);
+    enemy_type = random(2);
+  }
+
+  // collision check
+  if (enemy_pos == 0 && (enemy_type == 0 && player_pos < 4 || enemy_type == 1 && player_pos >= 3))
+  {
+    GameOver();
+    return;
+  }
+
   // draw screen
   for (int i = 0; i < 8; ++i) {
     for (int j = 0; j < 8; ++j) {
+      screen[i][j] = 0;
+    }
+  }
+  for (int i = 6; i < 8; ++i) {
+    for (int j = 0; j < 8; ++j) {
       screen[i][j] = 1;
     }
+  }
+  screen[8 - player_pos - 1][0] = 1;
+  screen[8 - player_pos][0] = 1;
+  if (enemy_pos >= 0 && enemy_pos < 8)
+  {
+    int row = (enemy_type == 0 ? 5 : 4);
+    screen[row][enemy_pos] = 1;
+  }
+}
+
+void GameOver() {
+  for (int t = 0; t < 999; ++t) {
+    for (int i = 0; i < 8; ++i) {
+      for (int j = 0; j < 8; ++j) {
+        screen[i][j] = t % 2;
+      }
+    }
+    Wait(40);
   }
 }
 
