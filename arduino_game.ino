72431320d1350b097a6d24cf7d531ab6f7433340
Marta Yurkevich <a32516@gmail.com>
2020-10-26 16:30:34 +0300

    Add button

--- arduino_game.ino
+++ arduino_game.ino
@@ -1,10 +1,17 @@
 int ROWS[] = {2, 7, A5, 5, 13, A4, 12, A2};
 int COLS[] = {6, 11, 10, 3, A3, 4, 8, 9};
+int LEFT_BUTTON = A0;
+int RIGHT_BUTTON = A1;
 
 unsigned char screen[8][8];
 
 void setup()
 {
+  Serial.begin(9600);
+  pinMode(LEFT_BUTTON, INPUT);
+  pinMode(RIGHT_BUTTON, INPUT);
+  digitalWrite(LEFT_BUTTON, HIGH);
+  digitalWrite(RIGHT_BUTTON, HIGH);
   for (int i = 0; i < 8; i++)
   {
     pinMode(ROWS[i], OUTPUT);
@@ -14,12 +21,19 @@ void setup()
 
 void loop()
 {
+  Update();
+  Wait(50);
+}
+
+void Update()
+{
+  Serial.println(digitalRead(LEFT_BUTTON));
+  // draw screen
   for (int i = 0; i < 8; ++i) {
     for (int j = 0; j < 8; ++j) {
       screen[i][j] = 1;
     }
   }
-  Wait(50);
 }
 
 void Wait(int t)
